5ae9634dcf10b56723da5ae4db6e79b3752d94db
GuidoZyl <guidozyl@gmail.com>
2023-03-16 21:01:26 -0300

    Made the calculation to know where the ball bounces without having to analyse the perspective, making the code faster

--- IA/prueba_perspectiva.py
+++ IA/prueba_perspectiva.py
@@ -9,10 +9,14 @@ pts2 = np.float32([[0, 0], [164, 0], [0, 474], [164, 474]])
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 deformado = cv2.warpPerspective(foto, matrix, (164, 474))
 
-print(matrix)
+cords_pelota = np.array([[237], [165], [1]])
+cords_pelota_pers = np.dot(matrix, cords_pelota)
+cords_pelota_pers = (int(cords_pelota_pers[0]/cords_pelota_pers[2]), int(cords_pelota_pers[1]/cords_pelota_pers[2]))
 
-#Hola Arotu!
+deformado = cv2.circle(deformado, cords_pelota_pers, 2, (0, 0, 255), -1)
+
+print(cords_pelota_pers)
 
 cv2.imshow('prueba', foto)
-cv2.imshow('deformado', deformado)
+#cv2.imshow('deformado', deformado)
 cv2.waitKey(0)
