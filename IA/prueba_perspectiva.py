9832e7ca8b58768ec3bc37be9f26f6bf081fc715
AryBacher <47170979@est.ort.edu.ar>
2023-03-20 13:19:44 -0300

    added an Inked photo

--- IA/prueba_perspectiva.py
+++ IA/prueba_perspectiva.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-foto = cv2.imread("E:\Guido\Documentos\Programacion\Hawkeye\IA\imagenPrueba.png")
+foto = cv2.imread("imagenPrueba.png")
 
 pts1 = np.float32([[121, 144], [271, 144], [64, 281], [341, 283]])
 pts2 = np.float32([[0, 0], [164, 0], [0, 474], [164, 474]])
@@ -18,5 +18,5 @@ deformado = cv2.circle(deformado, cords_pelota_pers, 2, (0, 0, 255), -1)
 print(cords_pelota_pers)
 
 cv2.imshow('prueba', foto)
-#cv2.imshow('deformado', deformado)
+cv2.imshow('deformado', deformado)
 cv2.waitKey(0)
