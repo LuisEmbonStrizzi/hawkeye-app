1c3f9543024e9a2461f51d84412f9fc549ddc160
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2023-05-31 20:03:27 -0300

    Delete test apps

--- interest-web-app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config')
-
-const nextConfig = {
-  reactStrictMode: false,
-  i18n,
-};
-
-module.exports = nextConfig;
