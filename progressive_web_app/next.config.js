ec702cc2bfbcf7e03fb7469249163652095639ed
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2023-05-31 23:29:56 -0300

    Conversion to pwa finished

---
+++ progressive_web_app/next.config.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  /* 
+    Deshabilitar la PWA en desarrollo pero habilitarla en producción. Para evitar sobrecarga en la consola.
+    disable: process.env.NODE_ENV === "development",
+  */
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: ["storage.googleapis.com", "storage.cloud.google.com"]
+  },
+})
