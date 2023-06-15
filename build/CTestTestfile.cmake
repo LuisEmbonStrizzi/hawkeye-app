aff58b7e1bcf6b40aa6240ad6728a768289d848d
GuidoZyl <guidozyl@gmail.com>
2023-06-14 23:48:09 -0300

    Remove unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/46919295/Documents/hawkeye-app/AI/C++
-# Build directory: C:/Users/46919295/Documents/hawkeye-app/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
