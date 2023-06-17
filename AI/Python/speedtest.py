cc79e7adc1facad64b1981fd84df4ca5c66141a6
GuidoZyl <guidozyl@gmail.com>
2023-06-17 14:24:16 -0300

    Pass an image from Python to C++ using pointers and modified it using OpenCV, then returned the result (finally!!)

--- AI/Python/speedtest.py
+++ AI/Python/speedtest.py
@@ -7,8 +7,8 @@ import ctypes
 #### CTYPES ####
 cpplibrary = ctypes.CDLL("./cpplibrary.so", winmode=0)
 
-resultado = cpplibrary.suma(1, 2)
-print(f"corrí en C++: {resultado}")
+#resultado = cpplibrary.suma(1, 2)
+#print(f"corrí en C++: {resultado}")
 
 resizer = 3
 greenLower = np.array([29, 50, 110])
@@ -17,7 +17,23 @@ greenUpper = np.array([64, 255, 255])
 imagen = np.zeros((1000,1000,3),np.uint8)
 imagen = cv2.putText(imagen, "Luis panza", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color=(0, 255, 0))
 cv2.imwrite('imagen.jpg', imagen)
-#cv2.imshow('luis trol', imagen)
+
+# Convierte la imagen a un array de NumPy
+imagen_array = np.asarray(imagen)
+
+# Obtiene un puntero al array NumPy
+imagen_ptr = ctypes.c_void_p(imagen_array.ctypes.data)
+
+# Crea una variable en Python para almacenar el retorno de la función en C++
+result = ctypes.c_int()
+
+# Llama a la función en C++
+cpplibrary.abrir_img(imagen_ptr, imagen.shape[0], imagen.shape[1], imagen.shape[2], ctypes.byref(result))
+
+# Accede al resultado a través del puntero
+result_value = result.value
+print("Resultado:", result_value)
+
 
 """
 if __name__ == '__main__':
