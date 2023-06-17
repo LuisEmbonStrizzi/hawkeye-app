02dbd8ac2dd5c1ee945ac0280f49348bd5e313f7
GuidoZyl <guidozyl@gmail.com>
2023-06-17 02:32:08 -0300

    Run C++ with OpenCV in a docker container!

--- AI/C++/main.cpp
+++ AI/C++/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 
 int main(int, char**) {
     Mat image;
-    image = imread("./AI/imagenPrueba.png");
+    image = imread("./imagenPrueba.png");
 
-    imshow("testeando...", image);
+    Point center(100, 100);
+    int radius = 50;
+    Scalar line_Color(0, 0, 0);
+    int thickness = 2;
+    circle(image, center,radius, line_Color, thickness);
+
+    imwrite("luis.jpg", image);
 
     return 0;
 }
