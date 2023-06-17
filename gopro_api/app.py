674c378d014055597eea94367dfd7256727608ee
Alan-Yeger <alanyeger1@gmail.com>
2023-06-17 17:24:00 -0300

    minor changes

--- gopro_api/app.py
+++ gopro_api/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from gopro_api import gopro
+from gopro_api import WirelessGoPro, Params
 app = FastAPI()
 
 @app.get("/connectCameras")
 async def connect_to_cameras():
+    with WirelessGoPro() as gopro:
+        gopro.ble_command.set_shutter(Params.Toggle.ENABLE)
     return {"message": "Cameras connected"}
 
 @app.get("/record")
