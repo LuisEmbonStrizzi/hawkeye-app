d72d03e6279dbe4b4c2ce3b2aa39d419839e3c17
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2023-06-19 22:19:59 -0300

    Styles applied to login page

--- progressive_web_app/src/pages/log-in.tsx
+++ progressive_web_app/src/pages/log-in.tsx
@@ -1,11 +1,9 @@
 import AuthForm from "~/components/AuthForm";
-import Link from "next/link";
 import { type NextPage } from "next";
 
 const LogIn: NextPage = () => {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center bg-[url('/img/bg-1920x1080.png')]">
-      <h1 className="font-bold  text-h1 text-foreground-important">Log In</h1>
+    <main className="flex flex-col gap-[24px] px-[16px] min-h-screen items-center justify-center bg-[url('/img/bg-1920x1080.png')]">
       <AuthForm mode="login" />
     </main>
   );
