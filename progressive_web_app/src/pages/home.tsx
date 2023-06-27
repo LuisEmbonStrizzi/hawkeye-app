73713c9536ff7b7c90439595d599df67d191e7e9
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2023-06-26 23:30:36 -0300

    Profile component finished

--- progressive_web_app/src/pages/home.tsx
+++ progressive_web_app/src/pages/home.tsx
@@ -1,29 +1,18 @@
 import { type NextPage } from "next";
-import { signOut, useSession, getSession } from "next-auth/react";
+import {getSession } from "next-auth/react";
 import type { GetServerSidePropsContext } from "next/types";
-import Button from "~/components/Button";
-import { api } from "~/utils/api";
+import Profile from "~/components/Profile";
 
 const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
   return (
-    <>
-      <div className="w-1/4 bg-secondary-background">
-        <div className="flex flex-col gap-[20px] p-20 text-center">
-          <p className="flex flex-col gap-[10px] text-center text-xl text-gray-500 ">
-            {sessionData && (
-              <span>
-                Logged in as{" "}
-                {sessionData.user?.name ||
-                  sessionData.user.email?.split("@")[0]}
-              </span>
-            )}
-            {secretMessage && <span> {secretMessage}</span>}
+    <main className="flex min-h-screen flex-col items-center justify-center gap-[24px] bg-background bg-no-repeat px-[32px] ">
+      <Profile />
+    </main>
+  );
+};
+
+/*
+
             {sessionData && (
               <Button
                 style="primary"
@@ -31,12 +20,9 @@ const Home: NextPage = () => {
                 onClick={() => void signOut({ callbackUrl: "/log-in" })}
               />
             )}
-          </p>
-        </div>
-      </div>
-    </>
-  );
-};
+
+*/
+
 
 export default Home;
 
