90393d9c4f3703f18bb4f7d193a26790d60ba528
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2023-09-07 22:42:18 -0300

    minor changes

--- gopro_api/official_sdk/main.py
+++ gopro_api/official_sdk/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 import requests
-from tutorial.tutorial_modules.tutorial_5_connect_wifi.wifi_enable import main as wifi_enable
+from tutorial.tutorial_modules.tutorial_5_connect_wifi.wifi_enable import enable_wifi
 from tutorial.tutorial_modules.tutorial_2_send_ble_commands.ble_command_set_shutter import main as ble_command_set_shutter
 from tutorial.tutorial_modules.tutorial_6_send_wifi_commands.send_wifi_command import main as send_wifi_command
-import sys
-import asyncio
-import argparse
 from tutorial_modules import GOPRO_BASE_URL, logger
 from tutorial_modules.tutorial_6_send_wifi_commands.wifi_command_get_media_list import get_media_list
 app = FastAPI()
@@ -13,74 +10,29 @@ app = FastAPI()
 
 @app.get("/connectCameras")
 async def connect_to_cameras():
-    parser = argparse.ArgumentParser(
-        description="Connect to a GoPro camera via BLE, get WiFi info, and enable WiFi."
-    )
-    parser.add_argument(
-        "-i",
-        "--identifier",
-        type=str,
-        help="Last 4 digits of GoPro serial number, which is the last 4 digits of the default \
-                camera SSID. If not used, first discovered GoPro will be connected to",
-        default=None,
-    )
-    parser.add_argument(
-        "-t",
-        "--timeout",
-        type=int,
-        help="time in seconds to maintain connection before disconnecting. If not set, will maintain connection indefinitely",
-        default=None,
-    )
-    args = parser.parse_args()
     try:
-        asyncio.run(wifi_enable(args.identifier, args.timeout))
-        return {"message": "Cameras connected"}
+        await enable_wifi()
+        return {"message": "Camera connected"}
 
     except Exception as e:  # pylint: disable=broad-exception-caught
         logger.error(e)
-        sys.exit(-1)
 
 
 @ app.get("/record")
 async def record():
-    parser = argparse.ArgumentParser(
-        description="Connect to a GoPro camera, set the shutter on, wait 2 seconds, then set the shutter off."
-    )
-    parser.add_argument(
-        "-i",
-        "--identifier",
-        type=str,
-        help="Last 4 digits of GoPro serial number, which is the last 4 digits of the default camera SSID. If not used, first discovered GoPro will be connected to",
-        default=None,
-    )
-    args = parser.parse_args()
-
     try:
         send_wifi_command("/gopro/camera/setting?setting=167&option=2")
-        asyncio.run(ble_command_set_shutter(args.identifier, [3, 1, 1, 1]))
+        await ble_command_set_shutter([3, 1, 1, 1])
         return {"message": "Record started"}
 
     except Exception as e:
         logger.error(e)
-        sys.exit(-1)
 
 
 @ app.get("/stopRecording")
 async def stopRecording():
-    parser = argparse.ArgumentParser(
-        description="Connect to a GoPro camera, set the shutter on, wait 2 seconds, then set the shutter off."
-    )
-    parser.add_argument(
-        "-i",
-        "--identifier",
-        type=str,
-        help="Last 4 digits of GoPro serial number, which is the last 4 digits of the default camera SSID. If not used, first discovered GoPro will be connected to",
-        default=None,
-    )
-    args = parser.parse_args()
-
     try:
-        asyncio.run(ble_command_set_shutter(args.identifier, [3, 1, 1, 1]))
+        await ble_command_set_shutter([3, 1, 1, 0])
         media_list = get_media_list()
         video = [x["n"] for x in media_list["media"][0]["fs"]][-1]
 
@@ -101,7 +53,6 @@ async def stopRecording():
 
     except Exception as e:
         logger.error(e)
-        sys.exit(-1)
 
 
 @ app.get("/getBattery")
