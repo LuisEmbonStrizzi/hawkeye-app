57abd594075bd358b4c826914967b81ef46b1fd5
GuidoZyl <guidozyl@gmail.com>
2023-09-11 21:17:40 -0300

    Solved conflict

--- AI/Python/prueba2.py
+++ AI/Python/prueba2.py
@@ -1,46 +1,39 @@
-import numpy as np
 import cv2
 
-vs = cv2.VideoCapture("../InkedInkedTennisBrothersVideo1080p.mp4")
+def zoomear_video(url_video : str, pique : list[(int, int), int]):
+    vs = cv2.VideoCapture(url_video)
 
-fps = 30
+    fps = int(vs.get(cv2.CAP_PROP_FPS))
 
-output_path = 'video_zoom.mp4'
-frame_width = int(vs.get(3))
-frame_height = int(vs.get(4))
-out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'avc1'), fps, (frame_width, frame_height))
-zooming = False
-zoom_coords = (962, 542) # Si y es mayor a 812 tira error (?????) 1732
-
-start_frame = 271  # Cambia este valor al fotograma en el que deseas iniciar el zoom
-zoom_duration = 50  # Cambia este valor a la duración de la animación de zoom en fotogramas
-
-unidad_x = (zoom_coords[0] - frame_width / 2) / zoom_duration
-unidad_y = (zoom_coords[1] - frame_height / 2) / zoom_duration
-
-num_frame = 0
-while True:
-    num_frame += 1
-
-    frame = vs.read()[1]
-
-    try:
-        if frame is None:
-            break
-
+    output_path = 'video_zoom.mp4'
+    frame_width = int(vs.get(3))
+    frame_height = int(vs.get(4))
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'avc1'), fps, (frame_width, frame_height)) # Con el códec XVID y ciertos valores tira error
     
+    zoom_coords = pique[0]
+    start_frame = pique[1]  # Frame del pique
+    zoom_duration = 50  # Duración en frames del zoom
+
+    unidad_x = (zoom_coords[0] - frame_width / 2) / zoom_duration
+    unidad_y = (zoom_coords[1] - frame_height / 2) / zoom_duration
+
+    num_frame = 0
+    while True:
+        num_frame += 1
+
         if num_frame < start_frame:
+            frame = vs.read()[1]
             out.write(frame)
+
         elif num_frame == start_frame:
             # Congela el último fotograma antes del inicio del zoom
+            frame = vs.read()[1]
             frozen_frame = frame.copy()
+
         elif num_frame <= start_frame + zoom_duration:
-            print("HOLA")
             zoom_factor = 1.0 + (num_frame - start_frame) / zoom_duration
-            print("zoom factor", zoom_factor)
             
             frame_zoomed = cv2.resize(frozen_frame, (int(frame_width * zoom_factor), int(frame_height * zoom_factor)))
-            zoom_coords_zoomed = (zoom_coords[0] * zoom_factor, zoom_coords[1] * zoom_factor)
             
             # Calcula el desplazamiento para centrar el zoom en el punto específico
             dx = int(unidad_x * (num_frame - start_frame) * zoom_factor + (frame_zoomed.shape[1] - frame_width) / 2)
@@ -48,44 +41,32 @@ while True:
 
             dx = max(0, min(dx, frame_zoomed.shape[1] - frame_width))
             dy = max(0, min(dy, frame_zoomed.shape[0] - frame_height))
+
+            # if dx < 0:
+            #     dx = 0
+
+            # elif dx + frame_width > frame_zoomed.shape[1]:
+            #     dx -= dx + frame_width - frame_zoomed.shape[1]
             
-            ###print("dx", dx)
-            #dy = int((zoom_coords_zoomed[1] - zoom_coords[1]) / 2)
-            ###print("dy", dy)
-            print("frame", (frame_zoomed.shape[1], frame_zoomed.shape[0]))
+            # if dy < 0:
+            #     dy = 0
 
-            if dx < 0:
-                print("ENTRE EN EL PRIMERO")
-                dx = 0
-
-            elif dx + frame_width > frame_zoomed.shape[1]:
-                print("ENTRE EN EL SEGUNDO")
-                dx -= dx + frame_width - frame_zoomed.shape[1]
-            
-            if dy < 0:
-                print("ENTRE EN EL TERCERO")
-                dy = 0
-
-            elif dy + frame_height > frame_zoomed.shape[0]:
-                print("ENTRE EN EL CUARTO")
-                dy -= dy + frame_height - frame_zoomed.shape[0]
-
-            print("dx", dx)
-            print("dy", dy)
+            # elif dy + frame_height > frame_zoomed.shape[0]:
+            #     dy -= dy + frame_height - frame_zoomed.shape[0]
 
             # Aplica el recorte para centrar el zoom en el punto específico
-            frame_zoomed = frame_zoomed[dy:dy+frame_height, dx:dx+frame_width]
-            print("DY + altura", dy+frame_height)
-            print("total", (frame_zoomed.shape[1], frame_zoomed.shape[0]), "\n")
-            out.write(frame_zoomed)  # Escribe los fotogramas durante la animación de zoom
-            print("GUARDE")
+            frame_zoomed = frame_zoomed[dy:dy + frame_height, dx:dx + frame_width]
+
+            out.write(frame_zoomed)
         else:
-            break  # Sale del bucle después de la animación de zoom
+            break
 
-    except cv2.error as e:
-        print("Error de OpenCV:", e)
-        break
+        if frame is None:
+            break
 
-vs.release()
-out.release()
-cv2.destroyAllWindows()
+    vs.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    zoomear_video("../InkedInkedTennisBrothersVideo1080p.mp4", [(1321, 587), 1380])
