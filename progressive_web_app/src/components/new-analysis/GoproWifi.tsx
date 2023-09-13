632b3f689122f60334b8d4769abce4de5862267b
ArielAlzo <110468040+ArielAlzo@users.noreply.github.com>
2023-09-13 12:01:24 -0300

    Copy to clipboard feature added

--- progressive_web_app/src/components/new-analysis/GoproWifi.tsx
+++ progressive_web_app/src/components/new-analysis/GoproWifi.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button";
 import { useCopyToClipboard } from "@uidotdev/usehooks";
+import { toast } from "react-hot-toast";
 
 type GoproWifiProps = {
   name: string;
@@ -20,23 +21,21 @@ const GoproWifi: React.FC<GoproWifiProps> = ({
   const goproName = name;
 
   return (
-    <div className="mx-auto my-auto flex h-screen w-full flex-col items-center justify-center gap-6 p-6">
-      <h1 className="text-3xl font-semibold text-foreground-important">
-        Connect to the Gopro&apos; Wifi Network
+    <div className="mx-auto my-auto max-w-xl flex h-screen w-full flex-col items-center justify-center gap-6 p-6">
+      <h1 className="text-3xl w-full font-semibold text-foreground-important">
+        Connect to <span className="text-primary">{goproName}</span> Network
       </h1>
       <p className="text-foreground">
         This will allow us to establish a connection with the camera. Please
-        copy the name and password and enter the Wifi Network before continuing.
+        copy the password and connect with the serialized network before continuing.
       </p>
-      <div className="flex items-center justify-center gap-4">
-        <input value={goproName} className="relative w-full rounded-lg border border-secondary-border bg-secondary-background p-[10px] pr-[40px] text-sm text-foreground-important outline-none transition-all duration-300 ease-out focus:border-primary focus:ring-2 focus:ring-primary/30" />
-        <Button style="secondary" onClick={void copyToClipboard(goproName)} />
+      <div className="w-full flex flex-col gap-4">
+        <div className="py-2 pl-4 pr-2 flex-1 flex-grow bg-background-border text-foreground rounded-md flex items-center justify-between gap-2">
+          {goproPassword}
+          <Button style="secondary" onClick={()=>{void copyToClipboard(goproPassword); toast.success("Network password copied to clipboard")}} label="Copy" />
+        </div>
+        <Button style="primary" label="Continue" onClick={firstOnClick} />
       </div>
-      <div className="relative">
-        <input className="relative w-full rounded-lg border border-secondary-border bg-secondary-background p-[10px] pr-[40px] text-sm text-foreground-important outline-none transition-all duration-300 ease-out focus:border-primary focus:ring-2 focus:ring-primary/30" />
-        <Button style="secondary" onClick={void copyToClipboard(goproPassword)} />
-      </div>
-      <Button style="primary" label="Continue" onClick={firstOnClick} />
     </div>
   );
 };
