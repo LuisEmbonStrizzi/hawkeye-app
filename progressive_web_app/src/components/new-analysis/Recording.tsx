1cf72a93ba07d6a1912d1140a6353c007d90fa1c
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2023-09-14 20:28:23 -0300

    Zoom feature implemented

--- progressive_web_app/src/components/new-analysis/Recording.tsx
+++ progressive_web_app/src/components/new-analysis/Recording.tsx
@@ -1,11 +1,54 @@
 import Button from "../Button";
 import CamBattery from "./CamBattery";
 import Counter from "./Counter";
+import { useState, useRef, useEffect } from "react";
+import clsx from "clsx";
 
 const Recording: React.FC = () => {
+  const [video, setVideo] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  let hasReachedZoomMoment = false;
+  const zoomMoment = 6.97;
+
+  useEffect(() => {
+    if (video && videoRef.current) {
+      setInterval(() => {
+        if (
+          videoRef.current &&
+          videoRef.current.currentTime >= zoomMoment &&
+          !hasReachedZoomMoment
+        ) {
+          videoRef.current.pause();
+          console.log(videoRef.current.currentTime);
+          console.log(hasReachedZoomMoment);
+        }
+      }, 10);
+    }
+  }, [video, hasReachedZoomMoment]);
+
+  const handleZoom = () => {
+    if (videoRef.current) {
+      hasReachedZoomMoment = true;
+      void videoRef.current.play();
+    }
+  };
+
+  const handleReplay = () => {
+    if (videoRef.current) {
+      hasReachedZoomMoment = false;
+      videoRef.current.currentTime = 0;
+      void videoRef.current.play();
+    }
+  };
+
   return (
     <div className="flex h-screen w-full">
-      <aside className="flex h-full w-96 flex-col border-r border-background-border">
+      <aside
+        className={clsx(
+          video ? "hidden md:flex" : "flex",
+          " h-full w-full flex-col border-background-border md:w-96 md:border-r"
+        )}
+      >
         <div className="flex flex-grow flex-col items-center justify-center px-8 py-16">
           <Counter />
         </div>
@@ -14,6 +57,7 @@ const Recording: React.FC = () => {
           <Button
             style="primary"
             label="Call hawkeye"
+            onClick={() => setVideo(true)}
             icon={
               <svg
                 width="20"
@@ -33,6 +77,7 @@ const Recording: React.FC = () => {
           <Button
             style="secondary"
             label="Finish recording"
+            onClick={() => setVideo(false)}
             icon={
               <svg
                 width="20"
@@ -55,32 +100,99 @@ const Recording: React.FC = () => {
           <CamBattery battery={91} />
         </section>
       </aside>
-      <section className="flex h-full w-full flex-col items-center justify-center gap-4">
-        <h1 className="text-3xl font-semibold text-foreground-important">
-          No hawkeye call yet
-        </h1>
-        <p className="text-foreground">
-          Here you will see the clip of the point you want to analyze.
-        </p>
+      <section
+        className={clsx(
+          video ? "flex" : "hidden md:flex",
+          "relative h-full w-full flex-col items-center justify-center gap-4 pt-[64px] "
+        )}
+      >
+        {video ? (
+          <>
+            <div className="absolute top-0 z-10 flex w-full items-center justify-between gap-4 border-b border-background-border bg-background p-8 py-[10px] md:justify-end">
+              {
+                <div className="md:hidden">
+                  <Button
+                    style="secondary"
+                    icon={
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12 15L7 10L12 5L13.0625 6.0625L9.125 10L13.0625 13.9375L12 15Z"
+                          className="fill-secondary-foreground transition-all duration-150 ease-out group-hover:fill-foreground"
+                        />
+                      </svg>
+                    }
+                    padding="icon"
+                    onClick={() => setVideo(false)}
+                  />
+                </div>
+              }
+              <div className="flex gap-4">
+                <Button
+                  style="primary"
+                  label="Zoom"
+                  iconPosition="left"
+                  onClick={handleZoom}
+                  icon={
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.5 17C4.0875 17 3.73437 16.8531 3.44062 16.5594C3.14687 16.2656 3 15.9125 3 15.5V12H4.5V15.5H8V17H4.5ZM12 17V15.5H15.5V12H17V15.5C17 15.9125 16.8531 16.2656 16.5594 16.5594C16.2656 16.8531 15.9125 17 15.5 17H12ZM3 8V4.5C3 4.0875 3.14687 3.73438 3.44062 3.44063C3.73437 3.14688 4.0875 3 4.5 3H8V4.5H4.5V8H3ZM15.5 8V4.5H12V3H15.5C15.9125 3 16.2656 3.14688 16.5594 3.44063C16.8531 3.73438 17 4.0875 17 4.5V8H15.5ZM9.99529 14C8.88732 14 7.94444 13.6095 7.16667 12.8286C6.38889 12.0477 6 11.1033 6 9.99529C6 8.88732 6.39046 7.94444 7.17138 7.16667C7.95229 6.38889 8.89674 6 10.0047 6C11.1127 6 12.0556 6.39046 12.8333 7.17138C13.6111 7.95229 14 8.89674 14 10.0047C14 11.1127 13.6095 12.0556 12.8286 12.8333C12.0477 13.6111 11.1033 14 9.99529 14ZM10 12.5C10.6944 12.5 11.2847 12.2569 11.7708 11.7708C12.2569 11.2847 12.5 10.6944 12.5 10C12.5 9.30556 12.2569 8.71528 11.7708 8.22917C11.2847 7.74306 10.6944 7.5 10 7.5C9.30556 7.5 8.71528 7.74306 8.22917 8.22917C7.74306 8.71528 7.5 9.30556 7.5 10C7.5 10.6944 7.74306 11.2847 8.22917 11.7708C8.71528 12.2569 9.30556 12.5 10 12.5Z"
+                        className="fill-background"
+                      />
+                    </svg>
+                  }
+                  padding="both-right"
+                />
+                <Button
+                  style="secondary"
+                  onClick={handleReplay}
+                  icon={
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10 18.125C9.02778 18.125 8.11713 17.9403 7.26806 17.5708C6.41898 17.2014 5.68009 16.7023 5.0514 16.0736C4.42269 15.4449 3.92361 14.706 3.55417 13.8569C3.18472 13.0079 3 12.0972 3 11.125H4.5C4.5 12.6389 5.03819 13.934 6.11458 15.0104C7.19097 16.0868 8.48611 16.625 10 16.625C11.5139 16.625 12.809 16.0868 13.8854 15.0104C14.9618 13.934 15.5 12.6389 15.5 11.125C15.5 9.61111 14.9618 8.31597 13.8854 7.23958C12.809 6.16319 11.5112 5.625 9.99187 5.625H9.875L10.9375 6.6875L9.875 7.75L7 4.875L9.875 2L10.9375 3.0625L9.85417 4.125H10C10.9722 4.125 11.8829 4.30972 12.7319 4.67917C13.581 5.04861 14.3199 5.54769 14.9486 6.1764C15.5773 6.80509 16.0764 7.54398 16.4458 8.39306C16.8153 9.24213 17 10.1528 17 11.125C17 12.0972 16.8153 13.0079 16.4458 13.8569C16.0764 14.706 15.5773 15.4449 14.9486 16.0736C14.3199 16.7023 13.581 17.2014 12.7319 17.5708C11.8829 17.9403 10.9722 18.125 10 18.125Z"
+                        className="fill-secondary-foreground transition-all duration-150 ease-out group-hover:fill-foreground"
+                      />
+                    </svg>
+                  }
+                  padding="icon"
+                />
+              </div>
+            </div>
+            <video autoPlay className="h-full" ref={videoRef} controls>
+              <source src="/videos/video_zoom_pro.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </>
+        ) : (
+          <>
+            {" "}
+            <h1 className="text-3xl font-semibold text-foreground-important">
+              No hawkeye call yet
+            </h1>
+            <p className="text-foreground">
+              Here you will see the clip of the point you want to analyze.
+            </p>
+          </>
+        )}
       </section>
     </div>
   );
 };
 export default Recording;
-/*
-
-        step === 1 ? (
-          <section className="flex flex-col gap-2 items-center justify-center my-auto">
-            <h1 className="text-3xl font-semibold text-foreground-important">
-              You are ready to start
-            </h1>{" "}
-            <p className="text-center text-foreground">
-              Everything is set up to start your new analysis with Hawkeye live.
-            </p>
-            <div className="absolute bottom-0 p-6 w-full flex flex-col sm:static sm:w-auto sm:p-0">
-              <Button label="Start  recording" style="primary" onClick={()=>handleStep("more")}/>
-            </div>
-          </section>
-        ) : (<div className="w-full h-full"></div>)
-
-*/
